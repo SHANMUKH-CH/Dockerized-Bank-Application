e8463b587f9db8cc4aa8e33f6e6d817de041c5e7
SHANMUKH-CH <shanmukhrodofatos@gmail.com>
2021-05-22 01:05:07 +0530

    sql connection

--- app.py
+++ app.py
@@ -1,65 +1,63 @@
+import mysql.connector
+import json
 from flask import Flask
+
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello():
-    return 'Hello World!'
-
+def hello_world():
+  return 'Hello, Docker!'
 
 @app.route('/widgets')
-def get_widgets():
-    mydb = mysql.connector.connect(
-        host="mysqldb",
-        user="root",
-        password="p@ssw0rd1",
-        database="inventory"
-    )
-    cursor = mydb.cursor()
+def get_widgets() :
+  mydb = mysql.connector.connect(
+    host="mysqldb",
+    user="root",
+    password="p@ssw0rd1",
+    database="inventory"
+  )
+  cursor = mydb.cursor()
 
-    cursor.execute("SELECT * FROM widgets")
 
-    # this will extract row headers
-    row_headers = [x[0] for x in cursor.description]
+  cursor.execute("SELECT * FROM widgets")
 
-    results = cursor.fetchall()
-    json_data = []
-    for result in results:
-        json_data.append(dict(zip(row_headers, result)))
+  row_headers=[x[0] for x in cursor.description] #this will extract row headers
 
-    cursor.close()
+  results = cursor.fetchall()
+  json_data=[]
+  for result in results:
+    json_data.append(dict(zip(row_headers,result)))
 
-    return json.dumps(json_data)
+  cursor.close()
 
+  return json.dumps(json_data)
 
 @app.route('/initdb')
 def db_init():
-    mydb = mysql.connector.connect(
-        host="mysqldb",
-        user="root",
-        password="p@ssw0rd1"
-    )
-    cursor = mydb.cursor()
+  mydb = mysql.connector.connect(
+    host="mysqldb",
+    user="root",
+    password="p@ssw0rd1"
+  )
+  cursor = mydb.cursor()
 
-    cursor.execute("DROP DATABASE IF EXISTS inventory")
-    cursor.execute("CREATE DATABASE inventory")
-    cursor.close()
+  cursor.execute("DROP DATABASE IF EXISTS inventory")
+  cursor.execute("CREATE DATABASE inventory")
+  cursor.close()
 
-    mydb = mysql.connector.connect(
-        host="mysqldb",
-        user="root",
-        password="p@ssw0rd1",
-        database="inventory"
-    )
-    cursor = mydb.cursor()
+  mydb = mysql.connector.connect(
+    host="mysqldb",
+    user="root",
+    password="p@ssw0rd1",
+    database="inventory"
+  )
+  cursor = mydb.cursor()
 
-    cursor.execute("DROP TABLE IF EXISTS widgets")
-    cursor.execute(
-        "CREATE TABLE widgets (name VARCHAR(255), description VARCHAR(255))")
-    cursor.close()
+  cursor.execute("DROP TABLE IF EXISTS widgets")
+  cursor.execute("CREATE TABLE widgets (name VARCHAR(255), description VARCHAR(255))")
+  cursor.close()
 
-    return 'init database'
-
+  return 'init database'
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+  app.run(host ='0.0.0.0')
